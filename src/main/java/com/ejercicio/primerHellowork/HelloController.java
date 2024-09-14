c41617bc25ddc776d20e49fc3088b749269dfc09
willzba <wlabarrera11@gmail.com>
2024-09-13 19:39:50 -0500

    primer CRUD

--- src/main/java/com/ejercicio/primerHellowork/HelloController.java
+++ src/main/java/com/ejercicio/primerHellowork/HelloController.java
@@ -1,27 +1,42 @@
 package com.ejercicio.primerHellowork;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * This class is a Spring Boot REST controller that handles HTTP GET requests to the "/hello" endpoint.
- * It provides two endpoints:
- *
- * - "/cartel": Returns a welcome message.
- * - "/hello": Returns a simple "Hello World" message.
- */
-@RestController
-@RequestMapping("/hello")
-public class HelloController {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-    @GetMapping("/cartel")
-    public String cartelBienvenida() {
-        return "Bienvenido a Spring Boot, Soy william";
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    private List<String> paises =new ArrayList<String>();
+
+    public HelloController(){
+        paises.add("Colombia");
+        paises.add("Peru");
+        paises.add("Brasil");
+        paises.add("Urugay");
+        paises.add("Venezuela");
+        paises.add("Paraguay");
+        paises.add("Bolivia");
     }
 
-    @GetMapping("/hello")
-    public String Hello() {
-        return "Hello Word.";
+    @GetMapping("/paises")
+    private List<String> getAllCountries(){
+
+        return paises;
+    }
+
+    @GetMapping("/paises/{id}")
+    public String getCountryById(@PathVariable int id){
+        if(id >=0 && id < paises.size()){
+            return paises.get(id);
+        }
+        else {
+            return "paises not found";
+        }
     }
 }
